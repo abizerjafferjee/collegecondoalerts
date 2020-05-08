f35324d0791c9198775b2d64eb0ef1767e3fa0be
Abizer Jafferjee <abizerjafferjee@gmail.com>
2020-05-08 16:20:43 +0000

    setup for crontab

--- webcrawler/setup/run_crawler.sh
+++ webcrawler/setup/run_crawler.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
-cd webcrawler
-scrapy crawler places4students
+#!/bin/bash
+cd /home/abizer/collegecondoalerts/webcrawler
+PATH=$PATH:/home/abizer/.local/bin
+export PATH
+scrapy crawl places4students
+#echo "Hello"
